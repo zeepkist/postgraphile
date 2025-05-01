84bea92ceb3c3e3eb79bc181209189a2d397b9ce
James Harris <3440094+wopian@users.noreply.github.com>
2025-05-02 00:01:19 +0100

    fix: update otel packages

--- src/otel.js
+++ src/otel.js
@@ -1,16 +1,16 @@
 import { NodeSDK } from "@opentelemetry/sdk-node"
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc"
-import { Resource } from "@opentelemetry/resources"
+import { resourceFromAttributes } from "@opentelemetry/resources"
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions"
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node"
 
 // Configure OTLP Exporter
 const traceExporter = new OTLPTraceExporter({
-	url: process.env.OPENTELEMETRY_COLLECTOR_URL, // Signoz OpenTelemetry Collector
+	url: process.env.OPENTELEMETRY_COLLECTOR_URL ?? 'postgraphile', // Signoz OpenTelemetry Collector
 })
 
 const sdk = new NodeSDK({
-	resource: new Resource({
+	resource: resourceFromAttributes({
 		[ATTR_SERVICE_NAME]: process.env.OPENTELEMETRY_SERVICE_NAME || "postgraphile",
 	}),
 	traceExporter,
