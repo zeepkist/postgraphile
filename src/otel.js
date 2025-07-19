0adeeb3628331b645388539b8f396591d937e9f1
James Harris <3440094+wopian@users.noreply.github.com>
2025-07-20 00:09:51 +0100

    feat: enable exporting metrics from postgraphile

--- src/otel.js
+++ src/otel.js
@@ -3,10 +3,17 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc"
 import { resourceFromAttributes } from "@opentelemetry/resources"
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions"
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node"
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc'
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
+
 
 // Configure OTLP Exporter
 const traceExporter = new OTLPTraceExporter({
-	url: process.env.OPENTELEMETRY_COLLECTOR_URL ?? 'postgraphile', // Signoz OpenTelemetry Collector
+	url: process.env.OPENTELEMETRY_COLLECTOR_URL ?? 'zeepkist-otel-collector:4317', // Signoz OpenTelemetry Collector
+})
+
+const metricExporter = new OTLPMetricExporter({
+	url: process.env.OPENTELEMETRY_COLLECTOR_URL ?? 'zeepkist-otel-collector:4317',
 })
 
 const sdk = new NodeSDK({
@@ -14,6 +21,10 @@ const sdk = new NodeSDK({
 		[ATTR_SERVICE_NAME]: process.env.OPENTELEMETRY_SERVICE_NAME || "postgraphile",
 	}),
 	traceExporter,
+	metricReader: new PeriodicExportingMetricReader({
+		exporter: metricExporter,
+		exportIntervalMillis: 60000, // Export metrics every 60 seconds
+	}),
 	instrumentations: [getNodeAutoInstrumentations()],
 })
 
